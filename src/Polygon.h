87c4826507ebc741726500d973c522bcfda147e3
tejal-patil <tejal.patil0606@gmail.com>
2025-04-29 19:46:02 -0700

    Update Polygon.h

--- src/Polygon.h
+++ src/Polygon.h
@@ -13,6 +13,9 @@ class Polygon {
 public:
     Polygon();
     Polygon(float x, float y, int sides, float length, float r, float g, float b);
+    float getX() const { return x; }
+    float getY() const { return y; }
+    float getSideLength() const { return length; }
 
     void draw();
 
